73aa7499722ab1aeeb4ee72a0c3ab96904e9ac00
brandn1337 <Demopaner10@gmail.com>
2021-03-31 09:28:52 -0400

    Hell yeaaa.

--- quests.js
+++ quests.js
@@ -1,16 +1,54 @@
 
+var Quest1 = 1;
+var Quest2;
+var Quest3;
+var Quest4;
+var Quest5;
+var Quest6;
 
 
+function CompleteQuest(Button)
+{
+	var finished;
+	switch(Button)
+	{
+		case 1:
+				finished = AllQuest(Quest1);
+				if (finished == true)
+					alert("Yay");
+				else
+					alert("Oh No");
+			break;
+		case 2:
+				finished = AllQuest(Quest2);
+			break;
+		case 3:
+				finished = AllQuest(Quest3);
+			break;
+		case 4:
+				finished = AllQuest(Quest4);
+			break;
+		case 5:
+				finished = AllQuest(Quest5);
+			break;
+		case 6:
+				finished = AllQuest(Quest6);
+			break;
+	}
 
-function CompleteQuest()
+}
+
+function UpdateQuest()
 {
 
 }
-function QuestUpdate(Whichquest)
+function AllQuest(CurrentQuest)
 {
-
-}
-function CheckQuests(Num)
-{
-
+	if(CurrentQuest == 1)
+	{
+		if(Stone => 20)
+			return true;
+		else
+			return false;
+	}
 }
